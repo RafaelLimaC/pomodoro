b431a6af41e102bff33e087b9a1013cb0fbd339f
RafaelLimaC <srafalimacosta@gmail.com>
2023-06-22 22:31:24 -0300

    Projeto finalizado

--- app/cronometro.js
+++ app/cronometro.js
@@ -2,33 +2,43 @@ let timerIdPomodoro;
 let timerIdIntervaloCurto;
 let timerIdIntervaloLongo;
 
-let isPausedPomodoro = true;
-let isPausedIntervaloCurto = true;
-let isPausedIntervaloLongo = true;
+let timerPropertiesPomodoro = {
+  isPaused: true,
+  totalTime: 1500,
+  timerId: null // 25min em segundos
+};
 
-let tempoTotalPomodoro = 1500; // 25min em segundos
-let tempoTotalIntervaloCurto = 300; // 5min em segundos
-let tempoTotalIntervaloLongo = 900; // 15min em segundos
+let timerPropertiesIntervaloCurto = {
+  isPaused: true,
+  totalTime: 300,
+  timerId: null // 5min em segundos
+};
+
+let timerPropertiesIntervaloLongo = {
+  isPaused: true,
+  totalTime: 900,
+  timerId: null // 15min em segundos
+};
 
 const valorInicialPomodoro = 1500;
-const valorInicialIntervaloCurto = 1500;
-const valorInicialIntervaloLongo = 1500;
+const valorInicialIntervaloCurto = 300;
+const valorInicialIntervaloLongo = 900;
 
-const timerPomodoro =       document.getElementById('timer-pomodoro');
+const timerPomodoro = document.getElementById('timer-pomodoro');
 const timerIntervaloCurto = document.getElementById('timer-intervalo-curto');
 const timerIntervaloLongo = document.getElementById('timer-intervalo-longo');
 
-const startButtonPomodoro =         document.getElementById('startButton-pomodoro');
-const startButtonIntervaloCurto =   document.getElementById('startButton-intervalo-curto');
-const startButtonIntervaloLongo =   document.getElementById('startButton-intervalo-longo');
+const startButtonPomodoro = document.getElementById('startButton-pomodoro');
+const startButtonIntervaloCurto = document.getElementById('startButton-intervalo-curto');
+const startButtonIntervaloLongo = document.getElementById('startButton-intervalo-longo');
 
-const pauseButtonPomodoro =         document.getElementById('pauseButton-pomodoro');
-const pauseButtonIntervaloCurto =   document.getElementById('pauseButton-intervalo-curto');
-const pauseButtonIntervaloLongo =   document.getElementById('pauseButton-intervalo-longo');
+const pauseButtonPomodoro = document.getElementById('pauseButton-pomodoro');
+const pauseButtonIntervaloCurto = document.getElementById('pauseButton-intervalo-curto');
+const pauseButtonIntervaloLongo = document.getElementById('pauseButton-intervalo-longo');
 
-const resetButtonPomodoro =         document.getElementById('resetButton-pomodoro');
-const resetButtonIntervaloCurto =   document.getElementById('resetButton-intervalo-curto');
-const resetButtonIntervaloLongo =   document.getElementById('resetButton-intervalo-longo');
+const resetButtonPomodoro = document.getElementById('resetButton-pomodoro');
+const resetButtonIntervaloCurto = document.getElementById('resetButton-intervalo-curto');
+const resetButtonIntervaloLongo = document.getElementById('resetButton-intervalo-longo');
 
 function formatTime(time) {
   let hours = Math.floor(time / 3600);
@@ -42,16 +52,17 @@ function formatTime(time) {
   }
 }
 
-function startTimer(start, pause, reset, tempoTotal, display, isPaused, timerId) {
-  isPaused = false;
+function startTimer(timerProperties, start, pause, reset, display, timerId) {
+  timerProperties.isPaused = false;
+  
   start.disabled = true;
   pause.disabled = false;
   reset.disabled = false;
-  
-  timerId = setInterval(function() {
-    tempoTotal--;
-    display.textContent = formatTime(tempoTotal);
-    if (tempoTotal <= 0) {
+
+  timerProperties.timerId = setInterval(function () {
+    timerProperties.totalTime--;
+    display.textContent = formatTime(timerProperties.totalTime);
+    if (timerProperties.totalTime <= 0) {
       clearInterval(timerId);
       start.disabled = false;
       pause.disabled = true;
@@ -60,57 +71,58 @@ function startTimer(start, pause, reset, tempoTotal, display, isPaused, timerId)
   }, 1000);
 }
 
-function pauseTimer(isPaused, start, pause, timerId) {
-  isPaused = true;
-  clearInterval(timerId);
+function pauseTimer(timerProperties, start, pause, timerId) {
+  timerProperties.isPaused = true;
+  clearInterval(timerProperties.timerId);
   start.disabled = false;
   pause.disabled = true;
 }
 
-function resetTimer(totalTime, display, start, pause, reset, timerId, valorInicial) {
-  clearInterval(timerId);
-  totalTime = valorInicial;
-  display.textContent = formatTime(totalTime);
+function resetTimer(timerProperties, display, start, pause, reset, timerId, initialValue) {
+  clearInterval(timerProperties.timerId);
+  timerProperties.totalTime = initialValue;
+  display.textContent = formatTime(timerProperties.totalTime);
   start.disabled = false;
   pause.disabled = true;
   reset.disabled = true;
 }
 
 function iniciarTimer(e) {
+  e.preventDefault();
   if (e.target.id === 'startButton-pomodoro') {
-    startTimer(startButtonPomodoro, pauseButtonPomodoro, resetButtonPomodoro, tempoTotalPomodoro, timerPomodoro, isPausedPomodoro, timerIdPomodoro);
+    startTimer(timerPropertiesPomodoro, startButtonPomodoro, pauseButtonPomodoro, resetButtonPomodoro, timerPomodoro, timerIdPomodoro);
   } 
   if (e.target.id === 'startButton-intervalo-curto') {
-    startTimer(startButtonIntervaloCurto, pauseButtonIntervaloCurto, resetButtonIntervaloCurto, tempoTotalIntervaloCurto, timerIntervaloCurto, isPausedIntervaloCurto, timerIdIntervaloCurto);
+    startTimer(timerPropertiesIntervaloCurto, startButtonIntervaloCurto, pauseButtonIntervaloCurto, resetButtonIntervaloCurto, timerIntervaloCurto, timerIdIntervaloCurto);
   } 
   if (e.target.id === 'startButton-intervalo-longo') {
-    startTimer(startButtonIntervaloLongo, pauseButtonIntervaloLongo, resetButtonIntervaloLongo, tempoTotalIntervaloLongo, timerIntervaloLongo, isPausedIntervaloLongo, timerIdIntervaloLongo);
+    startTimer(timerPropertiesIntervaloLongo, startButtonIntervaloLongo, pauseButtonIntervaloLongo, resetButtonIntervaloLongo, timerIntervaloLongo, timerIdIntervaloLongo);
   }
 }
 
 function ativarPause(e) {
+  e.preventDefault();
   if (e.target.id === 'pauseButton-pomodoro') {
-    pauseTimer(isPausedPomodoro, startButtonPomodoro, pauseButtonPomodoro, timerIdPomodoro);
+    pauseTimer(timerPropertiesPomodoro, startButtonPomodoro, pauseButtonPomodoro, timerIdPomodoro);
   } 
   if (e.target.id === 'pauseButton-intervalo-curto') {
-    pauseTimer(isPausedIntervaloCurto, startButtonIntervaloCurto, pauseButtonIntervaloCurto, timerIdIntervaloCurto)
+    pauseTimer(timerPropertiesIntervaloCurto, startButtonIntervaloCurto, pauseButtonIntervaloCurto, timerIdIntervaloCurto);
   } 
   if (e.target.id === 'pauseButton-intervalo-longo') {
-    pauseTimer(isPausedIntervaloLongo, startButtonIntervaloLongo, pauseButtonIntervaloLongo, timerIdIntervaloLongo);
+    pauseTimer(timerPropertiesIntervaloLongo, startButtonIntervaloLongo, pauseButtonIntervaloLongo, timerIdIntervaloLongo);
   }
 }
 
 function ativarReset(e) {
   e.preventDefault();
-
-  if (e.target.id === 'pauseButton-pomodoro') {
-    resetTimer(tempoTotalPomodoro, timerPomodoro, startButtonPomodoro, pauseButtonPomodoro, resetButtonPomodoro, timerIdPomodoro, valorInicialPomodoro);
+  if (e.target.id === 'resetButton-pomodoro') {
+    resetTimer(timerPropertiesPomodoro, timerPomodoro, startButtonPomodoro, pauseButtonPomodoro, resetButtonPomodoro, timerIdPomodoro, valorInicialPomodoro);
   } 
-  if (e.target.id === 'pauseButton-intervalo-curto') {
-    resetTimer(tempoTotalIntervaloCurto, timerIntervaloCurto, startButtonIntervaloCurto, pauseButtonIntervaloCurto, resetButtonIntervaloCurto, timerIdIntervaloCurto, valorInicialIntervaloCurto);
+  if (e.target.id === 'resetButton-intervalo-curto') {
+    resetTimer(timerPropertiesIntervaloCurto, timerIntervaloCurto, startButtonIntervaloCurto, pauseButtonIntervaloCurto, resetButtonIntervaloCurto, timerIdIntervaloCurto, valorInicialIntervaloCurto);
   } 
-  if (e.target.id === 'pauseButton-intervalo-longo') {
-    resetTimer(tempoTotalIntervaloLongo, timerIntervaloLongo, startButtonIntervaloLongo, pauseButtonIntervaloLongo, resetButtonIntervaloLongo, timerIdIntervaloLongo, valorInicialIntervaloLongo);
+  if (e.target.id === 'resetButton-intervalo-longo') {
+    resetTimer(timerPropertiesIntervaloLongo, timerIntervaloLongo, startButtonIntervaloLongo, pauseButtonIntervaloLongo, resetButtonIntervaloLongo, timerIdIntervaloLongo, valorInicialIntervaloLongo);
   }
 }
 
