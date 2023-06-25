b7d567c649ad683eba38f1fbc4af85b139726f2d
RafaelLimaC <srafalimacosta@gmail.com>
2023-06-25 00:23:37 -0300

    Concluindo to do list

--- app/toDoList.js
+++ app/toDoList.js
@@ -20,3 +20,82 @@ function ativarToDoList(element) {
     button.innerHTML = '<i class="fa-solid fa-angles-down"></i>To do list';
   }
 }
+
+/* javascript para inserir itens na lista */
+
+const form = document.getElementById('novoItem');
+const lista = document.getElementById("lista");
+const itens = JSON.parse(localStorage.getItem("itens")) || [];
+
+itens.forEach( (elemento) => {
+    criaElemento(elemento)
+})
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+
+    const nome = evento.target.elements['nome'];
+
+    const existe = itens.find( elemento => elemento.nome === nome.value);
+
+    
+    const itemAtual = {
+        "nome": nome.value,
+    }
+    
+    if (existe) {
+        itemAtual.id = existe.id;
+        atualizaElemento(itemAtual);
+
+        itens[itens.findIndex(elemento => elemento.id)] = itemAtual;
+
+    } else {
+        itemAtual.id = itens[itens.length -1] ? (itens[itens.length-1]).id + 1 : 0
+        criaElemento(itemAtual);
+        itens.push(itemAtual);
+    }
+
+    localStorage.setItem("itens", JSON.stringify(itens));
+
+    nome.value = "";
+});
+                            
+function criaElemento(item) {
+    const novoItem = document.createElement('li');
+    novoItem.classList.add("item");
+
+    const container = document.createElement('div');
+
+    const numeroDoItem = document.createElement('strong');
+    container.appendChild(numeroDoItem);
+
+    container.appendChild(document.createTextNode(item.nome));
+
+    novoItem.appendChild(container);
+
+    novoItem.appendChild(botaoDeleta(item.id));
+
+    lista.appendChild(novoItem);
+}
+
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
+}
+
+function botaoDeleta(id) {
+    const elementoBotao = document.createElement("button");
+    elementoBotao.innerText = "Deletar";
+
+    elementoBotao.addEventListener("click", function() {
+        deletaElemento(this.parentNode, id);
+    })
+    return elementoBotao;
+}
+
+function deletaElemento(tag, id) {
+    tag.remove();
+
+    itens.splice(itens.findIndex(elemento => elemento.id === id), 1);
+
+    localStorage.setItem("itens", JSON.stringify(itens));
+}
